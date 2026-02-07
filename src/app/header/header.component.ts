7e9bd94b2cfe4d35e11a715536a706627a9e4683
Alain Ghawi <aghawi@genetec.com>
2026-02-07 11:03:02 -0500

    Upgrade to v19

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
+    standalone: false
 })
 export class HeaderComponent {
   status = false;
