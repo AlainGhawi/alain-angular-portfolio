c186088dc43f76ef01fe248049ec66e23fe84b22
Alain Ghawi <ghawialain@gmail.com>
2026-06-01 08:02:16 -0400

    Push final changes for logos

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { afterNextRender, AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,4 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero.component.css'],
   standalone: false
 })
-export class HeroComponent {}
+export class HeroComponent {
+  private static readonly PROJECTS_COUNT = 16;
+  private static readonly CLIENTS_COUNT = 8;
+  private static readonly YEARS_COUNT = 10;
+
+  readonly animationDuration = 3000;
+
+  projects = 0;
+  clients = 0;
+  years = 0;
+
+  constructor() {
+    afterNextRender(() => {
+      this.animateValue(HeroComponent.PROJECTS_COUNT, v => this.projects = v);
+      this.animateValue(HeroComponent.CLIENTS_COUNT, v => this.clients = v);
+      this.animateValue(HeroComponent.YEARS_COUNT, v => this.years = v);
+    });
+  }
+
+  private animateValue(
+    end: number,
+    setter: (value: number) => void,
+    duration = this.animationDuration
+  ) {
+    const start = performance.now();
+
+    const update = (time: number) => {
+      const progress = Math.min((time - start) / duration, 1);
+      const eased = 1 - Math.pow(1 - progress, 3);
+
+      setter(Math.round(eased * end));
+
+      if (progress < 1) {
+        requestAnimationFrame(update);
+      }
+    };
+
+    requestAnimationFrame(update);
+  }
+}
